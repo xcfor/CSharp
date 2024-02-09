edb4a23dc7397b4eb1d7caa7969393cb246d1eb3
Chris Forbes <u1151925@umail.usq.edu.au>
2024-02-09 12:39:16 +1000

    Added more practice Csharp

---
+++ operators/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Operators
+{
+    internal class Program
+    {
+        // Main is the entry point for C# programs - this is a void function so no return
+        static void Main(string[] args)
+        {
+            // Declaring some basic value type variables
+            int x = 10, y = 5; // can declare multiple vars per line, if desired
+            string a = "abcd", b = "efgh";
+
+            // Basic mathematics operators include +, -, /, *
+            Console.WriteLine("-- Basic maths -- ");
+            Console.WriteLine((x / y) * x);
+            Console.WriteLine(a + b);  // this is concatenating two strings using the + operator
+
+            // Increment and decrement shorthand operators
+            Console.WriteLine("-- Shorthand -- ");
+            x++;
+            y--;
+            Console.WriteLine("x: {0}\ny: {1}", x, y);
+
+            a += b;
+            Console.WriteLine("a: {0}", a);
+
+            // Logical operators (making decisions and checking conditions)
+            Console.WriteLine("-- Logic operators -- ");
+
+            Console.WriteLine(x > y && y >= 5);  // will return true if the conditions are satisfied, otherwise false
+            Console.WriteLine(x > y || y >= 5);
+
+            Console.WriteLine("-- Null-coalescing operators -- ");
+            string str = null;
+            //The ?? operator uses the left operand if NOT null or the right operand if it IS null
+
+            Console.WriteLine(str ?? "Some other string");
+
+
+        }
+    }
+}
--- vars_data_types/Program.cs
+++ vars_data_types/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace VarsDataTypes
+{
+    internal class Program
+    {
+        // Main is the entry point for C# programs - this is a void function so no return
+        static void Main(string[] args)
+        {
+            // Declaring some basic value type variables
+            int i = 10;
+            float f = 2.0f;
+            decimal d = 10.0m;
+            bool b = true;
+            char c = 'c';
+
+            //Declaring a string
+            string str = "a string";
+
+            //Declaring an implicit value (var will let the compiler figure out he data type e.g. int or string)
+            var x = 10;
+            var z = "Hello!";
+
+            //Declaring an array of type int with a length of 5
+            int[] vals = new int[5];
+
+            string[] strs = { "one", "two", "three", "four" };
+
+            //Using a formatting string
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}", i, c, b, str, f, d, x, z);
+
+            //null means no value in C#
+            object obj = null;
+            Console.WriteLine(obj);
+
+            //Implicit conversion between types
+            long bignum;
+            bignum = i;   //we decleared i as an int earlier - it will be implictly converted
+
+            //Explicit conversion between types
+            float i_to_f = (float)i;   //here we are converting an int we declared earlier - i - to a float called i_to_f
+            Console.WriteLine("{0}", i_to_f);
+
+
+            int f_to_i = (int)f;    //here we are converting a float we declared earlier - f - to an int called f_to_i
+            Console.WriteLine("{0}", f_to_i);
+
+        }
+    }
+}
